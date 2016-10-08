0ea660e01f3c43a5fdf6f27317ca4e962a303737
turtle96 <angjazlyn@hotmail.com>
2016-10-08 21:55:32 +0800

    update documentation for FindCommand

--- src/main/java/seedu/taskell/logic/commands/FindCommand.java
+++ src/main/java/seedu/taskell/logic/commands/FindCommand.java
@@ -11,10 +11,10 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD 
-            + ": Finds all tasks whose description contain any of "
+            + ": Finds all tasks which description and tags contain any of "
             + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+            + "Example: " + COMMAND_WORD + " chicken bananas";
 
     private final Set<String> keywords;
 
