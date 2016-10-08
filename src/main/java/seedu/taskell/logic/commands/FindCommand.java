18741427c35dc044dc65cf52da612ae479e4cee9
turtle96 <angjazlyn@hotmail.com>
2016-10-08 21:59:44 +0800

    update documentation for FindCommand again

--- src/main/java/seedu/taskell/logic/commands/FindCommand.java
+++ src/main/java/seedu/taskell/logic/commands/FindCommand.java
@@ -3,8 +3,8 @@ package seedu.taskell.logic.commands;
 import java.util.Set;
 
 /**
- * Finds and lists all tasks in address book whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Finds and lists all tasks in task manager whose description and tags contains any of the argument keywords.
+ * Keyword matching is not case sensitive.
  */
 public class FindCommand extends Command {
 
@@ -12,7 +12,7 @@ public class FindCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD 
             + ": Finds all tasks which description and tags contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "the specified keywords and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " chicken bananas";
 
