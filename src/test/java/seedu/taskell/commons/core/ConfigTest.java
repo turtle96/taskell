43088d5c0f1d6bf852dd8da98bff99adae195074
turtle96 <angjazlyn@hotmail.com>
2016-10-11 18:14:37 +0800

    rename app title to Taskell

--- src/test/java/seedu/taskell/commons/core/ConfigTest.java
+++ src/test/java/seedu/taskell/commons/core/ConfigTest.java
@@ -16,7 +16,7 @@ public class ConfigTest {
 
     @Test
     public void toString_defaultObject_stringReturned() {
-        String defaultConfigAsString = "App title : Address App\n" +
+        String defaultConfigAsString = "App title : Taskell\n" +
                 "Current log level : INFO\n" +
                 "Preference file Location : preferences.json\n" +
                 "Local data file location : data/addressbook.xml\n" +
