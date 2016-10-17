2bd8788d4e0ae77c1e5d5411be72907a615c22d0
turtle96 <angjazlyn@hotmail.com>
2016-10-17 23:46:10 +0800

    create FindTagCommand

---
+++ src/main/java/seedu/taskell/logic/commands/FindTagCommand.java
@@ -0,0 +1,30 @@
+package seedu.taskell.logic.commands;
+
+import java.util.Set;
+
+/**
+ * Finds and lists all tasks in task manager whose description contains any of the argument keywords.
+ * Keyword matching is not case sensitive.
+ */
+
+public class FindTagCommand extends Command {
+
+    public static final String COMMAND_WORD = "find-tag";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all tasks whose description contain any of "
+            + "the specified keywords (not case-sensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " help study homework";
+
+    private final Set<String> keywords;
+
+    public FindTagCommand(Set<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public CommandResult execute() {
+        model.updateFilteredTaskListByAnyKeyword(keywords);
+        return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
+    }
+}
