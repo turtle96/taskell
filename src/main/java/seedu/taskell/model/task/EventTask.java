61ff6cedf4c687ae1127138ef4a04c5cd28f0177
DESKTOP-Q2E8UPT\Mamtha <mamtha3005@gmail.com>
2016-10-17 11:26:02 +0800

    Add floating,event and Deadline task

---
+++ src/main/java/seedu/taskell/model/task/EventTask.java
@@ -0,0 +1,27 @@
+package seedu.taskell.model.task;
+
+import java.util.Objects;
+
+import seedu.taskell.commons.util.CollectionUtil;
+import seedu.taskell.model.tag.UniqueTagList;
+
+/**
+ * Represents a Task in the task manager.
+ * Guarantees: details are present and not null, field values are validated.
+ */
+public class EventTask extends Task {
+
+    /**
+     * Every field must be present and not null.
+     */
+    public EventTask(Description description, TaskDate taskDate, TaskTime taskTime, TaskPriority taskPriority, UniqueTagList tags) {
+        super(description,taskDate,taskTime,taskPriority,tags);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ReadOnlyTask // instanceof handles nulls
+                && this.isSameStateAs((ReadOnlyTask) other));
+    }
+}
---
+++ src/main/java/seedu/taskell/model/task/FloatingTask.java
@@ -0,0 +1,34 @@
+package seedu.taskell.model.task;
+
+import java.util.Objects;
+
+import seedu.taskell.model.task.Description;
+import seedu.taskell.commons.util.CollectionUtil;
+import seedu.taskell.model.tag.UniqueTagList;
+
+/**
+ * Represents a Task in the task manager.
+ * Guarantees: details are present and not null, field values are validated.
+ */
+public class FloatingTask extends Task {
+
+    /**
+     * Every field must be present and not null.
+     */
+    public FloatingTask(Description description, TaskDate taskDate, TaskTime taskTime, TaskPriority taskPriority, UniqueTagList tags) {
+        super(description,null,null,taskPriority,tags);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ReadOnlyTask // instanceof handles nulls
+                && this.isSameStateAs((ReadOnlyTask) other));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Floating Task [Description: %s] [Priority: %s] [Tags: %s] ", this.getDescription(),this.getTaskPriority(),this.getTags());
+    }
+
+}
