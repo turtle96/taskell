5f8be3ed00a0c163184486595e0143269ef7f6c7
turtle96 <angjazlyn@hotmail.com>
2016-10-22 02:06:48 +0800

    edits to SaveStorageLocationCommand

--- src/main/java/seedu/taskell/logic/commands/SaveStorageLocationCommand.java
+++ src/main/java/seedu/taskell/logic/commands/SaveStorageLocationCommand.java
@@ -1,5 +1,6 @@
 package seedu.taskell.logic.commands;
 
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import seedu.taskell.commons.core.Config;
@@ -8,6 +9,7 @@ import seedu.taskell.commons.core.LogsCenter;
 import seedu.taskell.commons.events.model.TaskManagerChangedEvent;
 import seedu.taskell.commons.events.storage.StorageLocationChangedEvent;
 import seedu.taskell.model.ReadOnlyTaskManager;
+import seedu.taskell.storage.JsonConfigStorage;
 
 /** Saves current data file to new filepath.
  * */
@@ -22,15 +24,18 @@ public class SaveStorageLocationCommand extends Command {
             + "Parameters: FILEPATH (must be valid and already present on computer)\n"
             + "Example: " + COMMAND_WORD + "C:/Users/chicken/Desktop/cat";
     
-    public static final String MESSAGE_SUCCESS = "Data successfully saved to new location.";
+    private static final String MESSAGE_SUCCESS = "Data successfully saved to new location.";
+    private static final String MESSAGE_INVALID_PATH = "Filepath given is invalid.";
     
     private static Config config;
     private String newStorageFilePath;
     private ReadOnlyTaskManager taskManager;
+    private static JsonConfigStorage jsonConfigStorage;
     
     public SaveStorageLocationCommand(String newStorageFilePath) {
         this.newStorageFilePath = newStorageFilePath.trim().replace("\\", "/") + "/taskmanager.xml";
         logger.severe("New file path" + newStorageFilePath);
+        jsonConfigStorage = new JsonConfigStorage(Config.DEFAULT_CONFIG_FILE);
     }
     
     public static void setConfig(Config c) {
@@ -40,17 +45,33 @@ public class SaveStorageLocationCommand extends Command {
     @Override
     public CommandResult execute() {
         assert config != null;
-        
+        assert jsonConfigStorage != null;
+
         taskManager = model.getTaskManager();
         config.setTaskManagerFilePath(newStorageFilePath);
-        EventsCenter.getInstance().post(new StorageLocationChangedEvent(config));
+        indicateStorageLocationChanged();
         
-        indicateTaskManagerChanged();
+        try {
+            indicateTaskManagerChanged();
+        } catch (Exception e) {
+            return new CommandResult(MESSAGE_INVALID_PATH);
+        }
+        
+        try {
+            jsonConfigStorage.saveConfigFile(config);
+        } catch (IOException e) {
+            logger.severe("save to config json error");
+        }
         return new CommandResult(MESSAGE_SUCCESS);
     }
+
+    private void indicateStorageLocationChanged() {
+        assert config != null;
+        EventsCenter.getInstance().post(new StorageLocationChangedEvent(config));
+    }
     
-    /** Raises an event to indicate the model has changed */
     private void indicateTaskManagerChanged() {
+        assert taskManager != null;
         EventsCenter.getInstance().post(new TaskManagerChangedEvent(taskManager));
     }
 
