6f2b6018013d11f707cf76781c5900f1b432435f
turtle96 <angjazlyn@hotmail.com>
2016-10-22 16:26:46 +0800

    edit SaveStorageLocation logic sequence of saving data and writing to config.json also add error handling for invalid filepath

--- src/main/java/seedu/taskell/logic/commands/SaveStorageLocationCommand.java
+++ src/main/java/seedu/taskell/logic/commands/SaveStorageLocationCommand.java
@@ -6,10 +6,10 @@ import java.util.logging.Logger;
 import seedu.taskell.commons.core.Config;
 import seedu.taskell.commons.core.EventsCenter;
 import seedu.taskell.commons.core.LogsCenter;
-import seedu.taskell.commons.events.model.TaskManagerChangedEvent;
 import seedu.taskell.commons.events.storage.StorageLocationChangedEvent;
 import seedu.taskell.model.ReadOnlyTaskManager;
 import seedu.taskell.storage.JsonConfigStorage;
+import seedu.taskell.storage.Storage;
 
 /** Saves current data file to new filepath.
  * */
@@ -20,26 +20,35 @@ public class SaveStorageLocationCommand extends Command {
     
     public static final String COMMAND_WORD = "save";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Saves data file to new location specified.\n"
-            + "Parameters: FILEPATH (must be valid and already present on computer)\n"
-            + "Example: " + COMMAND_WORD + "C:/Users/chicken/Desktop/cat";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Saves data file to new location specified. "
+            + "New files can be auto-created as long as given directory is valid.\n"
+            + "Parameters: FILEPATH (must be valid)\n"
+            + "Example: " + COMMAND_WORD + " C:/Users/chicken/Desktop/cat";
     
     private static final String MESSAGE_SUCCESS = "Data successfully saved to new location.";
-    private static final String MESSAGE_INVALID_PATH = "Filepath given is invalid.";
+    private static final String MESSAGE_INVALID_PATH = "Filepath given is invalid. Filepath will be reset to old path.";
     
     private static Config config;
-    private String newStorageFilePath;
+    private String newStorageFilePath, oldStorageFilePath;
     private ReadOnlyTaskManager taskManager;
     private static JsonConfigStorage jsonConfigStorage;
+    private static Storage storage;
     
     public SaveStorageLocationCommand(String newStorageFilePath) {
+        this.oldStorageFilePath = config.getTaskManagerFilePath();
+        logger.info("Old file path: " + oldStorageFilePath);
+        
         this.newStorageFilePath = newStorageFilePath.trim().replace("\\", "/") + "/taskmanager.xml";
-        logger.severe("New file path" + newStorageFilePath);
+        logger.info("New file path: " + this.newStorageFilePath);
         jsonConfigStorage = new JsonConfigStorage(Config.DEFAULT_CONFIG_FILE);
     }
     
     public static void setConfig(Config c) {
         config = c;
+    }
+    
+    public static void setStorage(Storage s) {
+        storage = s;
     }
 
     @Override
@@ -48,20 +57,17 @@ public class SaveStorageLocationCommand extends Command {
         assert jsonConfigStorage != null;
 
         taskManager = model.getTaskManager();
+        
         config.setTaskManagerFilePath(newStorageFilePath);
         indicateStorageLocationChanged();
-        
         try {
-            indicateTaskManagerChanged();
-        } catch (Exception e) {
+            storage.saveTaskManager(taskManager, newStorageFilePath);
+        } catch (IOException e) {
+            handleInvalidFilePathException();
             return new CommandResult(MESSAGE_INVALID_PATH);
         }
         
-        try {
-            jsonConfigStorage.saveConfigFile(config);
-        } catch (IOException e) {
-            logger.severe("save to config json error");
-        }
+        saveToConfigJson();
         return new CommandResult(MESSAGE_SUCCESS);
     }
 
@@ -70,9 +76,28 @@ public class SaveStorageLocationCommand extends Command {
         EventsCenter.getInstance().post(new StorageLocationChangedEvent(config));
     }
     
-    private void indicateTaskManagerChanged() {
-        assert taskManager != null;
-        EventsCenter.getInstance().post(new TaskManagerChangedEvent(taskManager));
+    private void handleInvalidFilePathException() {
+        logger.info("Error writing to filepath. Handling data save exception.");
+        assert config != null;
+        
+        config.setTaskManagerFilePath(oldStorageFilePath);  //set back to old filepath
+        indicateStorageLocationChanged();
+        
+        try {
+            storage.saveTaskManager(taskManager, newStorageFilePath);
+        } catch (IOException e) {
+            logger.severe("Error saving task manager");
+        }
+        
+        saveToConfigJson();
+    }
+    
+    private void saveToConfigJson() {
+        try {
+            jsonConfigStorage.saveConfigFile(config);
+        } catch (IOException e) {
+            logger.severe("save to config json error");
+        }
     }
 
 }
