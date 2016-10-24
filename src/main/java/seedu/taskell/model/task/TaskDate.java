1331c738dd4ff4e5ccd241ad8260ff7e947bb9e4
“marcusngwj” <marcusngwj@msn.com>
2016-10-24 11:40:11 +0800

    Add method for getting next day

--- src/main/java/seedu/taskell/model/task/TaskDate.java
+++ src/main/java/seedu/taskell/model/task/TaskDate.java
@@ -466,6 +466,16 @@ public class TaskDate {
     public static String getTomorrowDate() {
         return LocalDate.now().plusDays(1).format(standardFormat);
     }
+    
+    public TaskDate getNextDay() throws IllegalValueException {
+        try {
+            LocalDate localDate = LocalDate.of(Integer.valueOf(getYear()), Integer.valueOf(getMonth()), Integer.valueOf(getDay()));
+            LocalDate nextDay = localDate.plusDays(1);
+            return new TaskDate(nextDay.format(standardFormat));
+        } catch (IllegalValueException e) {
+            throw new IllegalValueException(MESSAGE_TASK_DATE_CONSTRAINTS);
+        }
+    }
 
     /**
      * Returns a string representing the integer value of this year
