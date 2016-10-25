e39c88fa350c096ca79580247233e09a3ec6db31
turtle96 <angjazlyn@hotmail.com>
2016-10-25 23:52:40 +0800

    add test for calendar view

---
+++ src/test/java/guitests/ViewCalendarCommandTest.java
@@ -0,0 +1,17 @@
+package guitests;
+
+import org.junit.Test;
+
+public class ViewCalendarCommandTest extends TaskManagerGuiTest {
+    
+    @Test
+    public void viewCal() {
+        String expected = "Displayed calendar.";
+        
+        commandBox.runCommand("calendar");
+        assertResultMessage(expected);
+        
+        commandBox.runCommand("cal");
+        assertResultMessage(expected);
+    }
+}
