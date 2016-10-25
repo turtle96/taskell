e99539ab4cc9ee2a2a8603d80628c8ec1709d374
turtle96 <angjazlyn@hotmail.com>
2016-10-25 22:10:25 +0800

    create ViewCalendarCommand

---
+++ src/main/java/seedu/taskell/logic/commands/ViewCalendarCommand.java
@@ -0,0 +1,16 @@
+package seedu.taskell.logic.commands;
+
+public class ViewCalendarCommand extends Command {
+    
+    public static final String COMMAND_WORD = "calendar";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows calendar view on right panel.\n"
+            + "Example: " + COMMAND_WORD;
+
+    @Override
+    public CommandResult execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
