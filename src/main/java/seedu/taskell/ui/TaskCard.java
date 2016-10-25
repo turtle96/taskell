a0459eaeb97052b40fceb0b1e54df84feee4bab3
“marcusngwj” <marcusngwj@msn.com>
2016-10-25 15:50:44 +0800

    Add background colour to differentiate priority

--- src/main/java/seedu/taskell/ui/TaskCard.java
+++ src/main/java/seedu/taskell/ui/TaskCard.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import seedu.taskell.model.task.ReadOnlyTask;
 import seedu.taskell.model.task.Task;
+import seedu.taskell.model.task.TaskPriority;
 
 public class TaskCard extends UiPart{
 
@@ -52,11 +53,26 @@ public class TaskCard extends UiPart{
         startTime.setText(task.getStartTime().taskTime);
         endTime.setText(task.getEndTime().taskTime);
         
+        setCardPaneBackground();
+        setDateTimeVisibility();
+    }
+    
+    private void setDateTimeVisibility() {
         if (task.getTaskType().equals(Task.FLOATING_TASK)) {
             startDate.setVisible(false);
             endDate.setVisible(false);
             startTime.setVisible(false);
             endTime.setVisible(false);
+        }
+    }
+    
+    private void setCardPaneBackground() {
+        if (task.getTaskPriority().taskPriority.equals(TaskPriority.HIGH_PRIORITY)) {
+            cardPane.setStyle(TaskPriority.HIGH_PRIORITY_BACKGROUND);
+        } else if (task.getTaskPriority().taskPriority.equals(TaskPriority.MEDIUM_PRIORITY)) {
+            cardPane.setStyle(TaskPriority.MEDIUM_PRIORITY_BACKGROUND);
+        } else if (task.getTaskPriority().taskPriority.equals(TaskPriority.LOW_PRIORITY)) {
+            cardPane.setStyle(TaskPriority.LOW_PRIORITY_BACKGROUND);
         }
     }
 
