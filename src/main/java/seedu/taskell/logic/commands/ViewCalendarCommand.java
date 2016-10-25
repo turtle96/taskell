9590654c3a17cbfb679e63c6868c1f381f752703
turtle96 <angjazlyn@hotmail.com>
2016-10-25 23:18:09 +0800

    add execute() for view calendar

--- src/main/java/seedu/taskell/logic/commands/ViewCalendarCommand.java
+++ src/main/java/seedu/taskell/logic/commands/ViewCalendarCommand.java
@@ -1,16 +1,25 @@
 package seedu.taskell.logic.commands;
 
+import seedu.taskell.commons.core.EventsCenter;
+import seedu.taskell.commons.events.ui.DisplayCalendarViewEvent;
+
 public class ViewCalendarCommand extends Command {
     
     public static final String COMMAND_WORD = "calendar";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows calendar view on right panel.\n"
             + "Example: " + COMMAND_WORD;
+    
+    private static final String MESSAGE_SUCCESS = "Displayed calendar.";
 
     @Override
     public CommandResult execute() {
-        // TODO Auto-generated method stub
-        return null;
+        indicateDisplayCalendarView();
+        return new CommandResult(String.format(MESSAGE_SUCCESS));
+    }
+    
+    private void indicateDisplayCalendarView() {
+        EventsCenter.getInstance().post(new DisplayCalendarViewEvent());
     }
 
 }
