d2bf8836748aad9dc67e0e1a19d9d11e531de056
turtle96 <angjazlyn@hotmail.com>
2016-10-29 03:05:19 +0800

    refactor

--- src/main/java/seedu/taskell/logic/commands/ViewHistoryCommand.java
+++ src/main/java/seedu/taskell/logic/commands/ViewHistoryCommand.java
@@ -3,6 +3,8 @@ package seedu.taskell.logic.commands;
 
 import seedu.taskell.commons.core.EventsCenter;
 import seedu.taskell.commons.events.ui.DisplayListChangedEvent;
+import seedu.taskell.model.History;
+import seedu.taskell.model.HistoryManager;
 
 /** Lists a list of previous commands available for Undo operation
  * */
@@ -14,13 +16,18 @@ public class ViewHistoryCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Listed all commands available for undo.";
     
     private static ViewHistoryCommand self;
+    private History history;
+    
+    public ViewHistoryCommand() {
+        history = HistoryManager.getInstance();
+    }
     
     public static ViewHistoryCommand getInstance() {
-    	if (self == null) {
-    		self = new ViewHistoryCommand();
-    	}
-    	
-    	return self;
+        if (self == null) {
+            self = new ViewHistoryCommand();
+        }
+        
+        return self;
     }
 
     @Override
@@ -31,7 +38,7 @@ public class ViewHistoryCommand extends Command {
 
     public void indicateDisplayListChanged() {
         EventsCenter.getInstance().post(
-                new DisplayListChangedEvent(UndoCommand.getListOfCommandHistoryText()));
+                new DisplayListChangedEvent(history.getListCommandText()));
     }
 
 }
