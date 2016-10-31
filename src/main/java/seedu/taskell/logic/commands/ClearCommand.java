1434b048efbb87609a1a74374e0a8d0f1c88678b
turtle96 <angjazlyn@hotmail.com>
2016-11-01 00:22:44 +0800

    edit ClearCommand to show dialog for confirm

--- src/main/java/seedu/taskell/logic/commands/ClearCommand.java
+++ src/main/java/seedu/taskell/logic/commands/ClearCommand.java
@@ -1,5 +1,8 @@
 package seedu.taskell.logic.commands;
 
+import seedu.taskell.commons.core.EventsCenter;
+import seedu.taskell.commons.events.ui.ClearCommandInputEvent;
+import seedu.taskell.model.Model;
 import seedu.taskell.model.TaskManager;
 
 /**
@@ -8,15 +11,33 @@ import seedu.taskell.model.TaskManager;
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Task manager has been cleared!";
+    public static final String MESSAGE_SUCCESS = "Clear command executed.";
+    private static ClearCommand self;
 
     public ClearCommand() {}
 
+    public static ClearCommand getInstance() {
+        if (self == null) {
+            self = new ClearCommand();
+        }
+        
+        return self;
+    }
 
     @Override
     public CommandResult execute() {
         assert model != null;
-        model.resetData(TaskManager.getEmptyTaskManager());
+        raiseClearCommandInputEvent();
         return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    /** will be called from UiManager after confirmation of clear
+     * */
+    public void executeClear() {
+        model.resetData(TaskManager.getEmptyTaskManager());
+    }
+
+    private void raiseClearCommandInputEvent() {
+        EventsCenter.getInstance().post(new ClearCommandInputEvent());      
     }
 }
