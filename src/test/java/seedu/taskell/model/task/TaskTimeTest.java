04ba7ea3e15d7fc2fb6214d7fb49fbef7736a9b0
“marcusngwj” <marcusngwj@msn.com>
2016-10-31 17:38:00 +0800

    Update test name naming convention for taskTime

--- src/test/java/seedu/taskell/model/task/TaskTimeTest.java
+++ src/test/java/seedu/taskell/model/task/TaskTimeTest.java
@@ -16,33 +16,7 @@ import static org.junit.Assert.assertTrue;
 //@@author A0139257X
 public class TaskTimeTest {
     @Test
-    public void assertValidFormatBehaviourForTime() {
-        assertTrue(TaskTime.isValidTime(TaskTime.DEFAULT_START_TIME));
-        assertTrue(TaskTime.isValidTime(TaskTime.DEFAULT_END_TIME));
-        assertTrue(TaskTime.isValidTime("12am"));
-        assertTrue(TaskTime.isValidTime("1.30pm"));
-        assertTrue(TaskTime.isValidTime("1:40pm"));
-        assertTrue(TaskTime.isValidTime("1-30am"));
-        assertTrue(TaskTime.isValidTime("2:30Am"));
-        assertTrue(TaskTime.isValidTime("noW"));
-        
-        //Valid Noon
-        assertTrue(TaskTime.isValidTime("noon"));
-        assertTrue(TaskTime.isValidTime("afterNoon"));
-        assertTrue(TaskTime.isValidTime("12noon"));
-        assertTrue(TaskTime.isValidTime("12-noon"));
-        
-        //Valid Midnight
-        assertTrue(TaskTime.isValidTime("midnight"));
-        assertTrue(TaskTime.isValidTime("Mid-Night"));
-        assertTrue(TaskTime.isValidTime("12MidnIght"));
-        assertTrue(TaskTime.isValidTime("12-miDnight"));
-        assertTrue(TaskTime.isValidTime("12mid-night"));
-        assertTrue(TaskTime.isValidTime("12-mid-nighT"));
-    }
-
-    @Test
-    public void assertInvalidFormatBehaviourForTime() {
+    public void isValidTime_invalidTime_returnFalse() {
         assertFalse(TaskTime.isValidTime("1.3am"));
         assertFalse(TaskTime.isValidTime("2"));
         assertFalse(TaskTime.isValidTime("13pm"));
@@ -52,7 +26,37 @@ public class TaskTimeTest {
     }
     
     @Test
-    public void assertValidNewTaskTimeBehaviour() {
+    public void isValidTime_validTime_returnTrue() {
+        assertTrue(TaskTime.isValidTime(TaskTime.DEFAULT_START_TIME));
+        assertTrue(TaskTime.isValidTime(TaskTime.DEFAULT_END_TIME));
+        assertTrue(TaskTime.isValidTime("12am"));
+        assertTrue(TaskTime.isValidTime("1.30pm"));
+        assertTrue(TaskTime.isValidTime("1:40pm"));
+        assertTrue(TaskTime.isValidTime("1-30am"));
+        assertTrue(TaskTime.isValidTime("2:30Am"));
+        assertTrue(TaskTime.isValidTime("noW"));
+    }
+     
+    @Test
+    public void isValidTime_validNoon_returnTrue() {
+        assertTrue(TaskTime.isValidTime("noon"));
+        assertTrue(TaskTime.isValidTime("afterNoon"));
+        assertTrue(TaskTime.isValidTime("12noon"));
+        assertTrue(TaskTime.isValidTime("12-noon"));
+    }
+    
+    @Test
+    public void isValidTime_validMidnight_returnTrue(){
+        assertTrue(TaskTime.isValidTime("midnight"));
+        assertTrue(TaskTime.isValidTime("Mid-Night"));
+        assertTrue(TaskTime.isValidTime("12MidnIght"));
+        assertTrue(TaskTime.isValidTime("12-miDnight"));
+        assertTrue(TaskTime.isValidTime("12mid-night"));
+        assertTrue(TaskTime.isValidTime("12-mid-nighT"));
+    }
+
+    @Test
+    public void constructor_validTime_success() {
         try {
             TaskTime time = new TaskTime("now");
             TaskTime expected = new TaskTime(TaskTime.getTimeNow().toString());
@@ -71,7 +75,7 @@ public class TaskTimeTest {
     }
     
     @Test
-    public void assertInvalidNewTaskTimeBehaviour() {
+    public void constructor_invalidTime_failure() {
         try {
             TaskTime time = new TaskTime("NOT A VALID TIME");
         } catch (IllegalValueException ive) {
@@ -80,39 +84,53 @@ public class TaskTimeTest {
     }
     
     @Test
-    public void assertTimeIsBeforeBehaviour() throws IllegalValueException {
-            TaskTime timeIs12Am = new TaskTime("12am");
-            TaskTime timeIs12Pm = new TaskTime("12pm");
-            TaskTime timeNot12Am = new TaskTime("3am");
-            TaskTime timeNot12Pm = new TaskTime("3pm");
-            TaskTime time = new TaskTime(TaskTime.DEFAULT_END_TIME);
-            
-            //Correct Behaviour
-            assertTrue(timeIs12Am.isBefore(timeIs12Pm));
-            assertTrue(timeIs12Am.isBefore(timeNot12Am));
-            assertTrue(timeNot12Am.isBefore(timeIs12Pm));
-            assertTrue(timeNot12Pm.isBefore(time));
-            assertTrue(timeIs12Am.isBefore(time));
-            
-            //Incorrect Behaviour
-            assertFalse(timeIs12Am.isBefore(timeIs12Am));
-            assertFalse(timeIs12Pm.isBefore(timeIs12Am));
-            assertFalse(timeIs12Pm.isBefore(timeNot12Am));
-    }
-    
-    @Test
-    public void assertTimeIsAfterBehaviour() throws IllegalValueException {
+    public void isBefore_thisTimeBeforeGivenTime_success() throws IllegalValueException {
         TaskTime timeIs12Am = new TaskTime("12am");
         TaskTime timeIs12Pm = new TaskTime("12pm");
         TaskTime timeNot12Am = new TaskTime("3am");
         TaskTime timeNot12Pm = new TaskTime("3pm");
         TaskTime time = new TaskTime(TaskTime.DEFAULT_END_TIME);
         
-        //Correct Behaviour
+        assertTrue(timeIs12Am.isBefore(timeIs12Pm));
+        assertTrue(timeIs12Am.isBefore(timeNot12Am));
+        assertTrue(timeNot12Am.isBefore(timeIs12Pm));
+        assertTrue(timeNot12Pm.isBefore(time));
+        assertTrue(timeIs12Am.isBefore(time));
+    }
+    
+    @Test
+    public void isBefore_thisTimeBeforeGivenTime_failure() throws IllegalValueException {
+        TaskTime timeIs12Am = new TaskTime("12am");
+        TaskTime timeIs12Pm = new TaskTime("12pm");
+        TaskTime timeNot12Am = new TaskTime("3am");
+        TaskTime timeNot12Pm = new TaskTime("3pm");
+        TaskTime time = new TaskTime(TaskTime.DEFAULT_END_TIME);
+        
+        assertFalse(timeIs12Am.isBefore(timeIs12Am));
+        assertFalse(timeIs12Pm.isBefore(timeIs12Am));
+        assertFalse(timeIs12Pm.isBefore(timeNot12Am));
+    }
+    
+    @Test
+    public void isAfter_thisTimeAfterGivenTime_success() throws IllegalValueException {
+        TaskTime timeIs12Am = new TaskTime("12am");
+        TaskTime timeIs12Pm = new TaskTime("12pm");
+        TaskTime timeNot12Am = new TaskTime("3am");
+        TaskTime timeNot12Pm = new TaskTime("3pm");
+        TaskTime time = new TaskTime(TaskTime.DEFAULT_END_TIME);
+        
         assertTrue(timeIs12Pm.isAfter(timeIs12Am));
         assertTrue(timeIs12Pm.isAfter(timeNot12Am));
+    }
+
+    @Test
+    public void isAfter_thisTimeAfterGivenTime_failure() throws IllegalValueException {
+        TaskTime timeIs12Am = new TaskTime("12am");
+        TaskTime timeIs12Pm = new TaskTime("12pm");
+        TaskTime timeNot12Am = new TaskTime("3am");
+        TaskTime timeNot12Pm = new TaskTime("3pm");
+        TaskTime time = new TaskTime(TaskTime.DEFAULT_END_TIME);
         
-        //Incorrect Behaviour
         assertFalse(timeIs12Am.isAfter(timeIs12Am));
         assertFalse(timeIs12Am.isAfter(timeIs12Pm));
         assertFalse(timeIs12Am.isAfter(timeNot12Am));
@@ -121,7 +139,7 @@ public class TaskTimeTest {
     }
     
     @Test
-    public void assertCorrectExtractionOfTime() throws IllegalValueException {
+    public void getPartOfTime_success() throws IllegalValueException {
         TaskTime validTime = new TaskTime("4.35pm");
         
         assertEquals("4", validTime.getHour());
@@ -130,14 +148,14 @@ public class TaskTimeTest {
     }
     
     @Test
-    public void assertCorrectCurrentTime() {
+    public void getCurrentTime_success() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mma");
         LocalTime currTime = LocalTime.now();
         assertEquals(LocalTime.of(currTime.getHour(), currTime.getMinute()).format(dtf), TaskTime.getTimeNow().toString());
     }
     
     @Test
-    public void assertCorrectLocalTime() throws IllegalValueException {
+    public void getLocalTime_success() throws IllegalValueException {
         //Valid hour in the morning
         TaskTime time = new TaskTime("3am");
         LocalTime actual = time.getLocalTime();
@@ -164,7 +182,7 @@ public class TaskTimeTest {
     }
     
     @Test 
-    public void assertCorrectLocalDateTime() throws IllegalValueException {
+    public void getLocalDateTime_success() throws IllegalValueException {
         TaskDate givenDate = new TaskDate("1-1-2100");
         TaskTime time = new TaskTime("3am");
 
@@ -175,19 +193,26 @@ public class TaskTimeTest {
     }
     
     @Test
-    public void assertCorrectToString() throws IllegalValueException {
+    public void toString_success() throws IllegalValueException {
         TaskTime time = new TaskTime("5pm");
         assertEquals("5:00PM", time.toString());
     }
     
     @Test
-    public void assertEqualsBehaviour() throws IllegalValueException {
+    public void equals_returnTrue() throws IllegalValueException {
         TaskTime time = new TaskTime("12.30am");
         TaskTime sameTime= new TaskTime("12.30am");
         TaskTime differentTime= new TaskTime("3pm");
         
         assertEquals(time, time);
         assertEquals(time, sameTime);
+    }
+    
+    @Test
+    public void equals_returnFalse() throws IllegalValueException {
+        TaskTime time = new TaskTime("12.30am");
+        TaskTime sameTime= new TaskTime("12.30am");
+        TaskTime differentTime= new TaskTime("3pm");
         
         assertNotSame(time, differentTime);
         assertNotSame(time, "3am");
