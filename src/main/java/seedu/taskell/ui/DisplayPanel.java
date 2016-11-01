d013960baece0219e63671709f4b59abab0c6756
turtle96 <angjazlyn@hotmail.com>
2016-11-01 17:54:15 +0800

    fix exception error bug for adding children to DisplayPanel pane

--- src/main/java/seedu/taskell/ui/DisplayPanel.java
+++ src/main/java/seedu/taskell/ui/DisplayPanel.java
@@ -97,7 +97,7 @@ public class DisplayPanel extends UiPart {
     
     public void loadCalendar(AnchorPane placeholder) {
                
-        placeholder.getChildren().remove(displayTextArea);
+        placeholder.getChildren().clear();
         agenda = calendarView.getAgenda();
         FxViewUtil.applyAnchorBoundaryParameters(agenda, 0.0, 0.0, 0.0, 0.0);
         placeholder.getChildren().add(agenda);
