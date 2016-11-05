79605ffe77a510359e11746b9b4a4a53b4adc468
“marcusngwj” <marcusngwj@msn.com>
2016-11-05 11:38:41 +0800

    Update EventTask class to make decision for components

--- src/main/java/seedu/taskell/model/task/EventTask.java
+++ src/main/java/seedu/taskell/model/task/EventTask.java
@@ -1,9 +1,6 @@
 package seedu.taskell.model.task;
 
-import java.util.Objects;
-
 import seedu.taskell.commons.exceptions.IllegalValueException;
-import seedu.taskell.commons.util.CollectionUtil;
 import seedu.taskell.model.tag.UniqueTagList;
 
 //@@author A0139257X
@@ -15,24 +12,22 @@ public class EventTask extends Task {
     public static final String MESSAGE_EVENT_CONSTRAINTS = "Start date and time must be before end date and time"
             + "\nAll date and time should not before current time";
     
+    private String[] taskComponentArray;
+    private boolean[] hasTaskComponentArray;
+    
     public EventTask(String[] taskComponentArray, boolean[] hasTaskComponentArray, UniqueTagList tags) throws IllegalValueException {
         
-        TaskDate eventStartDate = new TaskDate(taskComponentArray[START_DATE]);
-        TaskDate eventEndDate = new TaskDate(taskComponentArray[END_DATE]);
-        TaskTime eventStartTime = new TaskTime(taskComponentArray[START_TIME]);
-        TaskTime eventEndTime = new TaskTime(taskComponentArray[END_TIME]);
-        RecurringType eventRecurringType = new RecurringType(taskComponentArray[RECURRING_TYPE]);
+        this.taskComponentArray = taskComponentArray;
+        this.hasTaskComponentArray = hasTaskComponentArray;
         
+        TaskDate eventStartDate = new TaskDate(this.taskComponentArray[START_DATE]);
+        TaskTime eventEndTime = new TaskTime(this.taskComponentArray[END_TIME]);
+        RecurringType eventRecurringType = new RecurringType(this.taskComponentArray[RECURRING_TYPE]);
         
-        if (TaskDate.isValidFullDate(taskComponentArray[START_DATE]) 
-                && eventStartDate.isBefore(TaskDate.getTodayDate())) {
-            throw new IllegalValueException(MESSAGE_EVENT_CONSTRAINTS);
-        } else if (TaskDate.isValidMonthAndYear(taskComponentArray[START_DATE])
-                && eventStartDate.isBefore(TaskDate.getTodayDate())) {
-            throw new IllegalValueException(MESSAGE_EVENT_CONSTRAINTS);
-        }
+        verifyStartDate(eventStartDate);
+        TaskTime eventStartTime = setStartTime(eventStartDate);
         
-        eventEndDate = autoAdjustEndDate(eventStartDate, eventEndDate, eventStartTime, eventEndTime);
+        TaskDate eventEndDate = setEndDate(eventStartDate, eventStartTime, eventEndTime);
         
         if (!isValidEventDuration(eventStartDate, eventEndDate, eventStartTime, eventEndTime)) {
             throw new IllegalValueException(MESSAGE_EVENT_CONSTRAINTS);
@@ -55,18 +50,108 @@ public class EventTask extends Task {
         
     }
     
+    private void verifyStartDate(TaskDate eventStartDate) throws IllegalValueException {
+        if (TaskDate.isValidFullDate(taskComponentArray[START_DATE]) 
+                && eventStartDate.isBefore(TaskDate.getTodayDate())) {
+            throw new IllegalValueException(MESSAGE_EVENT_CONSTRAINTS);
+        } else if (TaskDate.isValidMonthAndYear(taskComponentArray[START_DATE])
+                && eventStartDate.isBefore(TaskDate.getTodayDate())) {
+            throw new IllegalValueException(MESSAGE_EVENT_CONSTRAINTS);
+        }
+    }
+    
+    private TaskTime setStartTime(TaskDate eventStartDate) throws IllegalValueException {
+        if (eventStartDate.equals(TaskDate.getTodayDate()) && !hasTaskComponentArray[Task.START_TIME_COMPONENT]) {
+            return TaskTime.getTimeNow();
+        } else {
+            return new TaskTime(taskComponentArray[Task.START_TIME]);
+        }
+    }
+    
+    private TaskDate setEndDate(TaskDate eventStartDate, TaskTime eventStartTime, TaskTime eventEndTime) throws IllegalValueException {
+        
+        initialiseEndDate();
+        
+        TaskDate eventEndDate = adjustEndDateDependingOnDateForm(eventStartDate, eventStartTime, eventEndTime);
+        
+        eventEndDate = adjustEndDateDependingOnEndTime(eventStartDate, eventEndDate, eventStartTime, eventEndTime);
+        
+        return eventEndDate;
+    }
+    
     /**
-     * Every field must be present and not null.
-     * @throws IllegalValueException 
+     * Adjust end date when start and end date is the same but end time is before start time.
+     * Bring end date to the nexxt day
      */
-    /*public EventTask(Description description, String taskType, TaskDate startDate, 
-            TaskDate endDate, TaskTime startTime, TaskTime endTime, TaskPriority taskPriority, 
-            RecurringType recurringType, TaskStatus taskStatus, UniqueTagList tags) throws IllegalValueException {
+    private TaskDate adjustEndDateDependingOnEndTime(TaskDate eventStartDate, TaskDate eventEndDate, TaskTime eventStartTime, 
+            TaskTime eventEndTime) throws IllegalValueException {
+        if (!hasTaskComponentArray[END_DATE_COMPONENT] && eventStartDate.equals(eventEndDate) && eventEndTime.isBefore(eventStartTime)) {
+            eventEndDate = eventEndDate.getNextDay();
+        }
+        return eventEndDate;
+    }
+    
+    /**
+     * Adjustment is made to end date to make sure end date is never before start date
+     */
+    private TaskDate adjustEndDateDependingOnDateForm(TaskDate eventStartDate, TaskTime eventStartTime, 
+            TaskTime eventEndTime) throws IllegalValueException {
         
+        TaskDate eventEndDate = new TaskDate(taskComponentArray[Task.END_DATE]);
+        String userInputEndDate = taskComponentArray[END_DATE];
         
+        if (TaskDate.isValidFullDate(userInputEndDate) && eventEndDate.isBefore(eventStartDate)
+                || TaskDate.isValidMonthAndYear(userInputEndDate) && eventEndDate.isBefore(eventStartDate)) {
+            throw new IllegalValueException(MESSAGE_EVENT_CONSTRAINTS);
+        } else if (TaskDate.isValidDayOfWeek(userInputEndDate)) {
+            eventEndDate = determineDayInWeekGivenName(taskComponentArray, eventStartDate, eventEndDate);
+        } else if (TaskDate.isValidMonth(userInputEndDate)) {
+            eventEndDate = determineDayGivenMonthOrYear(taskComponentArray, eventStartDate, eventEndDate);
+        } else if (TaskDate.isValidDayAndMonth(userInputEndDate)) {
+            eventEndDate = determineDayGivenMonthOrYear(taskComponentArray, eventStartDate, eventEndDate);
+        } 
         
+        return eventEndDate;
+    }
+    
+    private void initialiseEndDate() {
+        if (!hasTaskComponentArray[Task.END_DATE_COMPONENT]) {
+            taskComponentArray[Task.END_DATE] = taskComponentArray[Task.START_DATE];
+        }
+    }
+    
+    /**
+     * Determine end date in the week with respect to start date
+     */
+    private TaskDate determineDayInWeekGivenName(String[] taskComponentArray, 
+            TaskDate eventStartDate, TaskDate eventEndDate) {
         
-    }*/
+        if (!eventEndDate.isAfter(eventStartDate)) {
+            try {
+                eventEndDate = eventEndDate.getNextWeek();
+            } catch (IllegalValueException e) {
+                return null;
+            }
+        }
+        return eventEndDate;
+    }
+    
+    /**
+     * Determine end date in the year with respect to start date
+     */
+    private TaskDate determineDayGivenMonthOrYear(String[] taskComponentArray, 
+            TaskDate eventStartDate, TaskDate eventEndDate) {
+        if (eventEndDate.isBefore(eventStartDate)) {
+            try {
+                eventEndDate = eventEndDate.getNextYear();
+            } catch (IllegalValueException e) {
+                return null;
+            }
+        }
+        return eventEndDate;
+    }
+    
+    //@@author
     
     //@@author A0148004R
     private boolean isValidRecurringEvent(TaskDate startDate, TaskDate endDate, RecurringType recurringType){
@@ -91,32 +176,15 @@ public class EventTask extends Task {
             return false;
         } else if (startDate.isAfter(endDate)) {
             return false;
-        } if (startDate.equals(today) && startTime.isBefore(currentTime)) { 
+        } else if (startDate.equals(today) && startTime.isBefore(currentTime)) { 
+            return false;
+        } else if (endDate.equals(today) && endTime.isBefore(currentTime)){
             return false;
         } else {
             return true;
         }
     }
     
-    /**
-     * Adjust the endDate such that it fits into the real-world context
-     * @throws IllegalValueException
-     */
-    private TaskDate autoAdjustEndDate(TaskDate startDate, TaskDate endDate, TaskTime startTime, TaskTime endTime) throws IllegalValueException {
-        TaskDate today = TaskDate.getTodayDate();
-        if (startDate.equals(endDate) && startTime.isAfter(endTime)) {
-            endDate = endDate.getNextDay();
-        } 
-        
-        else if((TaskDate.between(startDate, endDate) > -TaskDate.NUM_DAYS_IN_A_WEEK)&& (TaskDate.between(startDate, endDate) < 0)) {
-            System.out.println(TaskDate.between(startDate, endDate));
-            endDate = endDate.getNextWeek();
-        }
-        
-        
-        return endDate;
-    }
-
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
