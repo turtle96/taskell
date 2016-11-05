8eb691ab6c9da8b07198777c3ce64f16b1f1d419
“marcusngwj” <marcusngwj@msn.com>
2016-11-05 12:21:09 +0800

    remove unnecessary test

--- src/test/java/seedu/taskell/logic/LogicManagerTest.java
+++ src/test/java/seedu/taskell/logic/LogicManagerTest.java
@@ -176,8 +176,6 @@ public class LogicManagerTest {
         assertCommandBehavior(
                 "add Valid Description with startDate after endDate from 1-jan-2200 to 1-jan-2100", EventTask.MESSAGE_EVENT_CONSTRAINTS);
         assertCommandBehavior(
-                "add Valid Description with startTime before Today's current time at 12am", EventTask.MESSAGE_EVENT_CONSTRAINTS);
-        assertCommandBehavior(
                 "add Valid Description p/invalidPriority ", TaskPriority.MESSAGE_TASK_PRIORITY_CONSTRAINTS);
         assertCommandBehavior(
                 "add Valid Description p/0 p/1 ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
